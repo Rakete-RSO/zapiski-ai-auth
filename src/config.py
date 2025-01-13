7e53c583f21b7fe711ea8c8dcc30c8a826ed61e4
Vanja Stojanović <64472202+Vanja-S@users.noreply.github.com>
2025-01-13 12:15:46 +0100

    Update config.py

--- src/config.py
+++ src/config.py
@@ -8,6 +8,8 @@ DATABASE_URL = os.getenv("DATABASE_URL") or ""
 if DATABASE_URL == "":
     raise Exception("DATABASE_URL environment variable is not set")
 
+BILLING_API_URL = os.getenv("BILLING_API_URL") or ""
+
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM", "HS256")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 300))
